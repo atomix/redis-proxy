0bee07e2b76cb46b90104647d5507c2a65fbeef5
adibrastegarnia <adib.rastegarnia@gmail.com>
2020-04-01 16:10:55 -0700

    Add redis reconciler building blocks

--- pkg/controller/v1beta1/cluster/reconciler.go
+++ pkg/controller/v1beta1/cluster/reconciler.go
@@ -15,8 +15,12 @@
 package cluster
 
 import (
+	"context"
+
 	"github.com/atomix/kubernetes-controller/pkg/apis/cloud/v1beta1"
+	"github.com/onosproject/onos-lib-go/pkg/logging"
 	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	ctrlmgr "sigs.k8s.io/controller-runtime/pkg/manager"
 
@@ -27,6 +31,8 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sigs.k8s.io/controller-runtime/pkg/source"
 )
+
+var log = logging.GetLogger("controller", "cluster", "reconciler")
 
 // Add adds redis controller
 func Add(mgr ctrlmgr.Manager) error {
@@ -69,6 +75,50 @@ type Reconciler struct {
 }
 
 // Reconcile reconciles controller objects
-func (r *Reconciler) Reconcile(reconcile.Request) (reconcile.Result, error) {
-	panic("implement me")
+func (r *Reconciler) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+	log.Info("Reconciling Cluster")
+
+	// Fetch the Cluster instance
+	cluster := &v1beta1.Cluster{}
+	err := r.client.Get(context.TODO(), request.NamespacedName, cluster)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return reconcile.Result{}, nil
+		}
+		return reconcile.Result{Requeue: true}, err
+	}
+
+	v1beta1.SetClusterDefaults(cluster)
+
+	// Reconcile the redis database backend
+	err = r.reconcileRedisBackend(cluster)
+	if err != nil {
+		return reconcile.Result{}, err
+	}
+
+	// Reconcile the redis proxy
+	if cluster.Spec.Proxy != nil {
+		err = r.reconcileRedisProxy(cluster)
+		if err != nil {
+			return reconcile.Result{}, err
+		}
+	}
+
+	// Reconcile the cluster status
+	if err := r.reconcileStatus(cluster); err != nil {
+		return reconcile.Result{}, err
+	}
+	return reconcile.Result{}, nil
+}
+
+func (r *Reconciler) reconcileRedisProxy(cluster *v1beta1.Cluster) error {
+	panic("Implement me")
+}
+
+func (r *Reconciler) reconcileRedisBackend(cluster *v1beta1.Cluster) error {
+	panic("Implement me")
+}
+
+func (r *Reconciler) reconcileStatus(cluster *v1beta1.Cluster) error {
+	panic("Implement me")
 }
